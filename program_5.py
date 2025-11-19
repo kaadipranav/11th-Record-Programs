a4e4dfbe27d02e6e0703d16607beb6d871cac486
kaadipranav <kiwi092020@gmail.com>
2025-11-19 17:45:50 +0530

    Program 5 done

--- program_5.py
+++ program_5.py
@@ -1,2 +1,12 @@
 #Record Program -5
-#Library charges
+#Sum of series
+
+n = int(input("Enter the number of terms: "))
+sum = 1.0
+fact = 1
+
+for i in range(1,n+1):
+    fact *= i
+    sum += i/fact
+    
+print("Sum of the series is: ", sum)
