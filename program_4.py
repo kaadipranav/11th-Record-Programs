510e568e7203cf93b32886ac675d0b2dea55ae3f
kaadipranav <kiwi092020@gmail.com>
2025-11-19 17:44:17 +0530

    Program 4 done

--- program_4.py
+++ program_4.py
@@ -1,2 +1,29 @@
 #Record Program -4
-#Library charges
+#Armstrong numbers
+
+start=int(input("Enter the starting number: "))
+end = int(input("Enter the ending number: "))
+
+print(f"Armstrong numbers between {start} and {end} are: ")
+
+while start<=end:
+    num = start
+    temp = num
+    sum = 0
+    
+    count = 0
+    temp2 = num
+    while temp2 > 0:
+        count += 1
+        temp2 = temp2 // 10
+        
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** count
+        temp = temp // 10
+        
+    if sum == num:
+        print(num)
+    
+    start += 1
