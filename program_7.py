5774420294ecb994a83fd2a3491d0d3ec1e049f2
kaadipranav <kiwi092020@gmail.com>
2025-11-20 16:38:12 +0530

    Program 7 done

--- program_7.py
+++ program_7.py
@@ -1,2 +1,14 @@
 #Record Program -7
-#
+#Hollow Triangle
+
+rows = int(input("Enter the number of rows:"))
+
+for i in range(rows):
+    for j in range(rows - i - 1):
+        print(" ", end = "")
+    for j in range(2*i + 1):
+        if j == 0 or j == 2*i or i == rows - 1:
+            print("*", end = "")
+        else:
+            print(" ", end = "")
+    print()
