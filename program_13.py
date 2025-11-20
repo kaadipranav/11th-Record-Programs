27bf67e50178abd391e0f6d0f906a4bb8a80da94
kaadipranav <kiwi092020@gmail.com>
2025-11-20 16:45:59 +0530

    Program 13 done

--- program_13.py
+++ program_13.py
@@ -1,2 +1,24 @@
 #Record Program -13
-#
+#Nested Tuples
+
+# Example nested tuple (you can change it)
+nested = ((10, 20, 30), (40, 50), (60, 70, 80, 90))
+
+print("Nested Tuple:", nested)
+means = []
+
+for tup in nested:
+    total = 0
+    for num in tup:
+        total = total + num
+    mean = total / len(tup)
+    means.append(mean)
+    print("Mean of", tup, "=", mean)
+
+# Mean of all means
+sum_means = 0
+for m in means:
+    sum_means = sum_means + m
+overall_mean = sum_means / len(means)
+
+print("Mean of means =", overall_mean)
