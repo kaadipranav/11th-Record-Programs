2eced38a50b95f2b4a90693fc74fa7a9091237fa
kaadipranav <kiwi092020@gmail.com>
2025-11-20 16:40:19 +0530

    Program 8 done

--- program_8.py
+++ program_8.py
@@ -1,2 +1,10 @@
 #Record Program -8
-#
+#String Manipulation 1
+
+s = input("Enter a string: ")
+vowel_count = 
+
+for i in s:
+    if i in 'aeiouAEIOU':
+        vowel_count = vowel_count + 1
+print("Number of vowels =", vowel_count)
